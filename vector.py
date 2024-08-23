6d7eca32bb9044c08e7f19890179a6ebe92d3639
Andrei Mihai <andreimihai2305is@gmail.com>
2024-08-23 12:43:34 +0300

    added more functionality

--- vector.py
+++ vector.py
@@ -1,6 +1,20 @@
+import math
 
 class Vector2:
 
     def __init__(self, x : float = 0.0, y : float = 0.0) -> None:
         self.x : float = x
         self.y : float = y
+
+
+    def __str__(self) -> str:
+        return f"x: {self.x}, y: {self.y}"
+    
+    def sub(self, other):
+        return Vector2(self.x - other.x, self.y - other.y)
+
+    def abs(self):
+        return Vector2(math.fabs(self.x), math.fabs(self.y))
+
+    def square_dist(self, other) -> float:
+        return (other.x - self.x) ** 2 + (other.y - self.y) ** 2
